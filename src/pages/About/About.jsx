15f11f0fd673841370a75d1d8a983c783c397de9
Rajpal@123 <you@example.com>
2023-11-05 23:45:45 +0530

    Home Section added

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./About.css"
+
+const About = () => {
+  return (
+    <div className="about">
+        <div className="row">
+            <div className="col-md-6"></div>
+            <div className="col-md-6"></div>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import "./Home.css"
-import Typewriter from 'typewriter-effect';
+import React from "react";
+import "./Home.css";
+import Typewriter from "typewriter-effect";
+import Resume from "../../components/docs/Resume.txt"
 
 const Home = () => {
     return (
         <>
             <div className="container-fluid home">
                 <div className="container content">
-                    <h1>I am a dev</h1>
+                    <h1>I am a </h1>
+                    <h2>
+                        <Typewriter
+                            options={{
+                                strings: [
+                                    "React Developer!",
+                                    "MERN Stack Developer!",
+                                    "Full Stack Developer!",
+                                    "Freelancer!"
+                                ],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </h2>
+                    <div className="buttons">
+                        <button className="btn hire">Hire Me</button>
+                        <a className="btn cv" href={Resume}>Resume</a>
+                    </div>
                 </div>
             </div>
-            
         </>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
